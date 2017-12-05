28199f43e9819d5092f6c9d462675715f4a577a4
vintavi <vivihernandezramirez@gmail.com>
2017-12-05 16:41:21 +0100

    change of code_Firmata_arduino_Processing

--- 2sensors/Arduino2Sensors/Arduino2Sensors.ino
+++ 2sensors/Arduino2Sensors/Arduino2Sensors.ino
@@ -1,12 +1,22 @@
+#include <Bounce2.h>
 const int potiPin = 7;
 const int buttonPin = 4;
 
 int serialValue = A0;
 int buttonState = 0;
 
+//bouncer------------
+Bounce debouncer = Bounce();
+//-------------------
+
 void setup() {
   pinMode(potiPin, INPUT);
-  pinMode(buttonPin, INPUT);
+  //pinMode(buttonPin, INPUT);
+  //Bouncer----------
+  pinMode(buttonPin,INPUT_PULLUP);
+  // After setting up the button, setup the Bounce instance :
+  debouncer.attach(buttonPin);
+  debouncer.interval(5); // interval in ms, (5 IS THE STANDARD)
   
   Serial.begin(9600);
 
@@ -14,6 +24,15 @@ void setup() {
 }
 
 void loop() {
+
+  //BOUNCER------
+  // Update the Bounce instance :
+  debouncer.update();
+
+  // Get the updated value :
+  int value = debouncer.read();
+
+  
   serialValue = analogRead(potiPin);
   serialValue = map(serialValue, 0, 1023, 0, 255);
 
